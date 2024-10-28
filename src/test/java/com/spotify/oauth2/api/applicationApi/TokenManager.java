fe872c2ed52e8aeae5cbcb01096b66a3dbda8fbd
Natasha Kostovska <44337710+natekostovska@users.noreply.github.com>
2024-10-28 21:30:52 +0100

    token method changes

--- src/test/java/com/spotify/oauth2/api/applicationApi/TokenManager.java
+++ src/test/java/com/spotify/oauth2/api/applicationApi/TokenManager.java
@@ -24,6 +24,7 @@ public class TokenManager {
                 System.out.println("Token is good to use");
             }
         } catch (Exception e) {
+            e.printStackTrace();
             throw new RuntimeException("ABORT!!! Failed to get token");
         }
 
